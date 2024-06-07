a1d5c937b075fa8f8d540e5ec7485db21c9fb3d3
Manuel García <manuel.garcia@zooplus.com>
2024-06-07 13:47:44 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import List from "./tasks/List";
 import Editor from "./tasks/Editor";
 import {Redis} from "@upstash/redis";
@@ -8,12 +8,33 @@ const redis = new Redis({
   token: process.env.REACT_APP_UPSTASH_REDIS_REST_TOKEN,
 });
 
-// const redis = Redis.fromEnv();
 
 const App = () => {
   const [tasks, setTasks] = useState(fetchTasks)
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let cachedData: any = await redis.get('my-data-key');
+        if (!cachedData) {
+          cachedData = fetchTasks();
+          await redis.set('my-data-key', JSON.stringify(cachedData), { ex: 3600 });
+        } else {
+          cachedData = JSON.parse(cachedData);
+        }
+        setTasks(cachedData)
+      } catch (err: any) {
+        console.log(err.message);
+      } finally {
+        // setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   function fetchTasks() {
+
     return [
       {title: 'my task 1', description: 'This is my task 1', status: false},
       {title: 'my task 2', description: 'This is my task 2', status: true},
